50430c56d5f9b99e9aa49d7f28f73459c0161462
abrhamadddis <abrhamaddis32@gmail.com>
2023-02-24 23:52:45 +0300

    element of javascript

---
+++ jsx-tutorial/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  const greeting = "grreeting";
+  const diplayfun = event => alert(event.target.id)
+  return(
+
+  );
+}
+
+export default App;
