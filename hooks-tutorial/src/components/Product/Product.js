53b5237bb87e82bdf4bee1007e51012a87f660ae
abrhamadddis <abrhamaddis32@gmail.com>
2023-03-17 18:24:00 +0300

    hooks

--- hooks-tutorial/src/components/Product/Product.js
+++ hooks-tutorial/src/components/Product/Product.js
@@ -5,24 +5,57 @@ const currencyOptions = {
   minimumFractionDigits: 2,
   maximumFractionDigits: 2,
 }
+const products = [
+  {
+    emoji: '🍦',
+    name: 'ice cream',
+    price: 5
+  },
+  {
+    emoji: '🍩',
+    name: 'donuts',
+    price: 2.5,
+  },
+  {
+    emoji: '🍉',
+    name: 'watermelon',
+    price: 4
+  }
+];
+
+function getTotal(total) {
+  return total.toLocaleString(undefined, currencyOptions)
+}
 
 export default function Product() {
   const [cart, setCart] = useState([]);
   const [total, setTotal] = useState(0);
 
-  function getTotal(total) {
-    return total.toLocaleString(undefined, currencyOptions)
+  function add() {
+    setCart(current => [...current, Product.name]);
+    setTotal(current => current + Product.price);
   }
-
+  function remove() {
+    setCart (current => []);
+    setTotal(current => current - Product.price);
+  }
   return(
     <div className="wrapper">
       <div>
         Shopping Cart: {cart.length} total items.
       </div>
-      <div>Total: {getTotal(total)}</div>
-
-      <div className="product"><span role="img" aria-label="ice cream">🍦</span></div>
-      <button>Add</button> <button>Remove</button>
+      <div>Total:{getTotal(total)}</div>
+        <div>
+        {products.map(product => (
+          <div key={product.name}>
+            <div className="product">
+              <span role="img" aria-label={product.name}>{product.emoji}</span>
+            </div>
+            <button onClick={add}>Add</button>
+            <button>Remove</button>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
